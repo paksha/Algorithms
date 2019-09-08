acd26b627fd5cce5f1c980677db2618d569d0087
Akshay Patel <akshaypatel07@hotmail.com>
2019-09-08 11:01:23 -0400

    Add files via upload

--- LCProblems/Palindrome Number.java
+++ LCProblems/Palindrome Number.java
@@ -1,17 +1,13 @@
-// Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
-// Negatives do not count since -121 does not read the same backwards (121-).
-
-// Akshay Patel
-
 class Solution {
     public boolean isPalindrome(int x) {
-        String number = Integer.toString(x);
-        int length = number.length();
-        for (int i = 0, j = length-1; (i < length && i <= j); i++, j--) {
-            if (number.charAt(i) != number.charAt(j)) {
-                return false;
-            }
+        if (x < 0) return false;
+        if (x < 10) return true;
+        int number = x;
+        long val = 0;
+        while (x != 0) {
+            val = val*10 + x%10;
+            x /= 10;
         }
-        return true;
+        return number == (int) val;
     }
 }
