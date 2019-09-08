6baf99d0be54aafa6839d857839e7211fd7d1a77
Akshay Patel <akshaypatel07@hotmail.com>
2019-09-07 20:39:23 -0400

    Add files via upload

--- LCProblems/Add Two Numbers.java
+++ LCProblems/Add Two Numbers.java
@@ -8,44 +8,37 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
         ListNode head = new ListNode(0);
         ListNode current = head;
         boolean add1 = false;
-        
-        while (l1!=null || l2!=null) {
+        while (l1 != null || l2 != null) {
             int sum = 0;
-            
-            if (l1==null) {
+            if (l1 == null) {
                 sum = l2.val;
                 l2 = l2.next;
             } else if (l2 == null) {
                 sum = l1.val;
                 l1 = l1.next;
             } else {
-                sum = l1.val+l2.val;
+                sum = l1.val + l2.val;
                 l1 = l1.next;
                 l2 = l2.next;
             }
-            
-            if (add1) {
-                sum++;
-            }
-            
-            if (sum>= 10) {
+            if (add1) sum += 1;
+            if (sum >= 10) {
                 sum -= 10;
                 add1 = true;
             } else {
                 add1 = false;
             }
-            
             current.next = new ListNode(sum);
             current = current.next;
         }
-        
         if (add1) {
             current.next = new ListNode(1);
+            current = current.next;
         }
-        
         return head.next;
     }
 }
